618c3320edc9c2ca997d9f3e5fceb1af678fe2a4
Pavel Bocharov <skidyschggwp@gmail.com>
2024-06-15 20:00:14 +0400

    Changes user button to render the right one if user is authenticated or not

--- frontend/podofcast/components/Header.tsx
+++ frontend/podofcast/components/Header.tsx
@@ -4,30 +4,38 @@ import Link from "next/link";
 import { StyledLogo } from "./StyledSVG";
 import { Button } from "./ui/button";
 import { HeaderLinks } from "@/constants";
+import useAuthStore from "@/store/useAuthStore";
+import UserMenuButton from "./UserMenuButton";
 
 const Header = () => {
+  const { isAuthenticated } = useAuthStore();
   return (
     <header className="fixed z-50 pt-6 bg-background rounded-bl-3xl rounded-br-3xl container mx-auto flex justify-between">
       <Link href="/">
         <StyledLogo />
       </Link>
       <ul className="flex gap-[60px] items-center leading-[1.6] font-bold">
-        {HeaderLinks.map(item => (
+        {HeaderLinks.map((item) => (
           <Link
-          href={item.link}
-          className="hover:text-primary transition-colors duration-300 ease-in-out"
-        >
-          <li>{item.name}</li>
-        </Link>
+            key={item.name}
+            href={item.link}
+            className="hover:text-primary transition-colors duration-300 ease-in-out"
+          >
+            <li>{item.name}</li>
+          </Link>
         ))}
       </ul>
       <div className="flex gap-5 items-center">
         <Button variant="outline" className="button--light" asChild>
           <Link href="#">RECENT EPISODES</Link>
         </Button>
-        <Button className="button" asChild>
-          <Link href="/sign-in">SIGN IN</Link>
-        </Button>
+        {isAuthenticated ? (
+          <UserMenuButton />
+        ) : (
+          <Button className="button" asChild>
+            <Link href="/sign-in">SIGN IN</Link>
+          </Button>
+        )}
       </div>
     </header>
   );
---
+++ frontend/podofcast/components/UserMenuButton.tsx
@@ -0,0 +1,56 @@
+import useAuthStore from "@/store/useAuthStore";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { useLogout } from "@/lib/reactQuery/authMutations";
+
+const UserMenuButton = () => {
+  const { user } = useAuthStore();
+  const { mutate } = useLogout();
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  return (
+    <div className="relative">
+      <Button
+        className="button"
+        onClick={() => setIsDropdownVisible(!isDropdownVisible)}
+      >
+        {user?.name || user?.email}
+      </Button>
+      <div
+        className={`overflow-hidden w-full absolute right-0 top-16 p-5 bg-secondary rounded-lg transition-all duration-300 ease-in-out ${
+          isDropdownVisible
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-full pointer-events-none"
+        }`}
+      >
+        <ul className="flex flex-col gap-4 items-center">
+          <li>
+            <Button
+              variant="outline"
+              className={`button--light transition-transform duration-300 ease-in-out delay-50 ${
+                isDropdownVisible ? "translate-x-0" : "translate-x-[200%]"
+              }`}
+              asChild
+            >
+              <Link href="/profile/me">Profile</Link>
+            </Button>
+          </li>
+          <li>
+            <Button
+              variant="outline"
+              className={`button--light transition-transform duration-300 ease-in-out delay-100 ${
+                isDropdownVisible ? "translate-x-0" : "translate-x-[200%]"
+              }`}
+              onClick={() => mutate()}
+            >
+              Logout
+            </Button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenuButton;
