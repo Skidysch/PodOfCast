05a936eb8b19239e38c9c7c3ff7d317f97b08c19
Pavel Bocharov <skidyschggwp@gmail.com>
2024-06-16 20:04:14 +0400

    Centers info pages due to the unified layout

--- frontend/podofcast/app/(auth)/password/reset/done/page.tsx
+++ frontend/podofcast/app/(auth)/password/reset/done/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="max-w-[630px] min-h-[540px] flex flex-col justify-center gap-5">
+    <div className="max-w-[630px] min-h-[540px] mx-auto flex flex-col justify-center gap-5">
       <h1 className="text-6xl text-center font-bold leading-tight tracking-tight">
         Your password has been set!
       </h1>
--- frontend/podofcast/app/(auth)/sign-up/confirm/page.tsx
+++ frontend/podofcast/app/(auth)/sign-up/confirm/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const confirmationEmail = useAuthStore((state) => state.confirmationEmail);
 
   return (
-    <div className="max-w-[630px] min-h-[540px] flex flex-col justify-center gap-5">
+    <div className="max-w-[630px] min-h-[540px] mx-auto flex flex-col justify-center gap-5">
       <h1 className="text-6xl text-center font-bold leading-tight tracking-tight">
         Thank you for joining us!
       </h1>
