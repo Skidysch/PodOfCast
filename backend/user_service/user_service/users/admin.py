2e88f3476bc28f38cc81d6e23ad4e9356e8a4ec7
Pavel Bocharov <skidyschggwp@gmail.com>
2024-06-22 16:54:22 +0400

    Refactor imports

--- backend/user_service/user_service/users/admin.py
+++ backend/user_service/user_service/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from .models import User
+from users.models import User
 
 
 class UserAdmin(BaseUserAdmin):
